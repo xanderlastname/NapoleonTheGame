d25ffa43506397ec278106f72cd261b7a6e1b0a8
xanderlastname <26xshotz@woodward.edu>
2023-11-18 19:37:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ class Player:
     def __init__(self, health, points):
         self.health = health
         self.points = points
+
+class Event:
+    def __init__(self, name, health, *attacks):
+        self.name = name
+        self.health = health
+        self.attacks = attacks
+
+def question(*options, prompt):
+    print(prompt)
+    print("Please choose an option:")
+    for option in options:
+        current = options.index(prompt) + 1
+        print(f"{current}. {option}")
