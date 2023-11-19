8436034cdf4247b831abc8378530be6444338f69
xanderlastname <26xshotz@woodward.edu>
2023-11-18 19:24:02 -0500

    test
    added prompt function (not finishewd jsut testiung)

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ class Player:
     def __init__(self, health, points):
         self.health = health
         self.points = points
+
+
+def question(*prompts, question):
+    lenPrompt = len(prompts)
+    print(question)
+    for prompt in prompts:
+        print(prompt)
+
+question('Yes', 'No', 'maybe', question="Do you want to do this")
