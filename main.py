f2fa19d935f9626f107ff0d285daf80bff5c2d39
xanderlastname <26xshotz@woodward.edu>
2023-11-18 20:34:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+from time import sleep
+
 #main python file
 #in cmd paste "python main.py"
 
@@ -12,9 +15,30 @@ class Event:
         self.health = health
         self.attacks = attacks
 
+# this function is used for intro and info it just makes it look better, copied and pasted from internet lol 0.05 delay is good
+def typewrite(*paragraph: str, delay) -> None:
+    for sentence in paragraph:
+        for char in sentence:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            sleep(delay)
+        print()
+        sleep(delay)
+
 def question(*options, prompt):
     print(prompt)
-    print("Please choose an option:")
+    current = 0
     for option in options:
-        current = options.index(prompt) + 1
+        current += 1
         print(f"{current}. {option}")
+
+    choice = input(f"Please choose an option from 1-{current}")
+    return choice
+
+# main is used to order and call everything else, it is automatically called when the file is run
+def main():
+    # points go to -10 you lose, its an indication of the correctness of your decisionmaking
+    player = Player(health=100, points=0)
+
+if __name__ == '__main__':
+    main()
